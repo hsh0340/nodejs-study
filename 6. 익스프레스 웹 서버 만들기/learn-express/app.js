3f6456eb1823a53dffc3632e0de4b74a62e94562
hsh0340 <hsh0340@naver.com>
2022-10-24 22:18:43 +0900

    🧑‍💻 20221024

--- 6. 익스프레스 웹 서버 만들기/learn-express/app.js
+++ 6. 익스프레스 웹 서버 만들기/learn-express/app.js
@@ -6,8 +6,12 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -25,17 +29,58 @@ app.use(session({
   name: 'session-cookie',
 }))
 
-app.use((req, res, next) => {
-  console.log('모든 요청에 다 실행됩니다.');
-  next();
-})
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
-app.get('/', (req, res, next) => {
-  console.log('GET / 요청에서만 실행됩니다.');
-  next();
-}, (req, res) => {
-  throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
 });
+
+const multer = require('multer');
+const fs = require('fs');
+
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads/');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024},
+});
+
+app.get('/uploads', (req, res) => {
+  res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+
+app.post('/upload', 
+  upload.fields([{ name: 'image1'}, { name: 'image2'}]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send('ok');
+  },  
+);
+
+// app.use((req, res, next) => {
+//   console.log('모든 요청에 다 실행됩니다.');
+//   next();
+// })
+
+// app.get('/', (req, res, next) => {
+//   console.log('GET / 요청에서만 실행됩니다.');
+//   next();
+// }, (req, res) => {
+//   throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+// });
 
 app.use((err, req, res, next) => {
   console.error(err);
